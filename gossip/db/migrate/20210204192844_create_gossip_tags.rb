3fded4fb66ec6a2d46f3041882c81d0994097506
Alexandre GAUTHIER <44195741+LX-pro-dev@users.noreply.github.com>
2021-02-05 10:18:07 +0100

    Update 20210204192844_create_gossip_tags.rb

--- gossip/db/migrate/20210204192844_create_gossip_tags.rb
+++ gossip/db/migrate/20210204192844_create_gossip_tags.rb
@@ -1,8 +1,8 @@
 class CreateGossipTags < ActiveRecord::Migration[6.1]
   def change
     create_table :gossip_tags do |t|
-      t.belongs_to :tags, index: true
-      t.belongs_to :gossip_cools, index: true
+      t.belongs_to :tag, index: true
+      t.belongs_to :gossip_cool, index: true
       t.timestamps
     end
   end
