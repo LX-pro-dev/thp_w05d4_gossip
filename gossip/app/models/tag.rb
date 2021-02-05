d6be5bf84ce8c1c22ddeab6795e993e917b7724c
Alexandre GAUTHIER <44195741+LX-pro-dev@users.noreply.github.com>
2021-02-05 10:14:57 +0100

    Update tag.rb

--- gossip/app/models/tag.rb
+++ gossip/app/models/tag.rb
@@ -1,6 +1,6 @@
 class Tag < ApplicationRecord
-  belongs_to :users, optional:true
-  belongs_to :gossips, optional:true
+  belongs_to :user, optional:true
+  belongs_to :gossip_cool, optional:true
   has_many :gossip_tags
-  has_many :gossips, through: :gossip_tags
+  has_many :gossip_cools, through: :gossip_tags
 end
